ed5c62aafd8d431e72cb268f20113bfa32d2edb8
alexisivr <alexisveron1993@gmail.com>
2017-06-10 17:26:07 -0300

    enviar mail de ocntacto

---
+++ cotizar.php
@@ -0,0 +1,201 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!-->
+ <!--<![endif]-->
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="Transpreb S.A. camiones viajes transportes transportación">
+  <meta name="author" content="Esta pagina contiene información sobre empresa de transportes de todo tipo alrededor de todo el pais.">
+
+    <title>TRANSPREB S.A.</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Google Web Font -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom CSS-->
+    <link href="css/general.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Owl-Carousel -->
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- Magnific Popup core CSS file -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <script src="js/modernizr-2.8.3.min.js"></script>  <!-- Modernizr /-->
+    <!--[if IE 9]>
+        <script src="js/PIE_IE9.js"></script>
+    <![endif]-->
+    <!--[if lt IE 9]>
+        <script src="js/PIE_IE678.js"></script>
+    <![endif]-->
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+    <!--
+    <div id="slogan">
+        <img src="Fotos/dsfdf.jpg"/>
+    </div>
+    <div class="clear"></div>-->
+    <div id="contact" class="content-section-a">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-6 col-md-offset-3 text-center wrap_title">
+                    <h2>Rellene los campos.</h2>
+                    <p class="lead" style="margin-top:0">Apenas podamos, nos comunicaremos.</p>
+                </div>
+
+                <form role="form" action="enviarMail.php" method="post">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="InputName">Nombre</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Ingrese un nombre" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputEmail">Correo</label>
+                            <div class="input-group">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese una dirección de correo" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="InputNumber">Teléfono</label>
+                            <div class="input-group">
+                                <input type="number" class="form-control" id="tel" name="tel" placeholder="Ingrese un teléfono" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputMessage">Mensaje</label>
+                            <div class="input-group">
+                                <textarea name="message" id="message" class="form-control" rows="5" required></textarea>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <hr class="featurette-divider hidden-lg">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="InputProd">Producto</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="prod" id="prod" placeholder="Ingrese el producto" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputValue">Valor declarado</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="value" name="value" placeholder="Ingrese el valor declarado" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="InputPeso">Peso</label>
+                            <div class="input-group">
+                                <input type="number" class="form-control" id="weight" name="weight" placeholder="Ingrese el peso en kilos" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputVolumen">Volumen</label>
+                            <div class="input-group">
+                                <input type="number" class="form-control" id="vol" name="vol" placeholder="Ingrese el volumen" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <hr class="featurette-divider hidden-lg" />
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="InputLocal1">Origen</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="local1" id="local1" placeholder="Ingrese la localidad de origen" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputProv1">Origen</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="prov1" id="prov1" placeholder="Ingrese la provincia" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="InputLocal2">Destino</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="local2" id="local2" placeholder="Ingrese la localidad" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Inputprov2">Destino</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="prov2" id="prov2" placeholder="Ingrese la provincia" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+
+                        <input type="submit" name="submit" id="submit" value="Enviar" class="btn wow tada btn-embossed btn-primary pull-right">
+                    </div>
+                </form>
+
+
+
+                <!--Social + texto comun-->
+                <hr class="featurette-divider hidden-lg">
+                <div class="col-md-5 col-md-push-1 address">
+                    <address>
+                        <h3>Oficina Administrativa</h3>
+                        <p class="lead">
+                            <a href="https://www.google.com/maps/preview?ie=UTF-8&q=The+Pentagon&fb=1&gl=us&hq=1400+Defense+Pentagon+Washington,+DC+20301-1400&cid=12647181945379443503&ei=qmYfU4H8LoL2oATa0IHIBg&ved=0CKwBEPwSMAo&safe=on">
+                                Villa Constitución<br>
+                                Santa Fe, Argentina
+                            </a><br>
+                            Teléfono: +54 3400 474001 | +54 3400 47606 |
+                            <br> +54 3400 476907
+                            <br>info@transprebsa.com.ar
+                            <br>
+                        </p>
+                    </address>
+
+                    <h3>Social</h3>
+                    <li class="social">
+                        <a href="#"><i class="fa fa-facebook-square fa-size"> </i></a>
+                    </li>
+                </div>
+            </div>
+            <div class="col-md-5 col-md-push-1 address" style="float:right;"> Vuelva a la página principal haciendo click <a href="index.html" style="color:blue;">aquí</a></div>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ enviarMail.php
@@ -0,0 +1,28 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+    $tel = intval($_POST['tel']);
+		$message = $_POST['message'];
+		$prod = intval($_POST['prod']);
+    $value = intval($_POST['weight']);
+    $weight = intval($_POST['weight']);
+    $vol = intval($_POST['vol']);
+    $local1 = intval($_POST['local1']);
+    $prov1 = intval($_POST['prov1']);
+    $local2 = intval($_POST['local2']);
+    $prov2 = intval($_POST['prov2']);
+		$from = 'testeando';
+		$to = 'alexisveron1993@gmail.com';
+		$subject = 'Presupuesto transporte ';
+
+		$body ="De: $name\n E-Mail: $email\n Mensaje:\n $message";
+// If there are no errors, send the email
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Gracias por contactarse! Responderemos a la brevedad </div>';
+	} else {
+		$result='<div class="alert alert-danger"> Hubo un inconveniente. Intente nuevamente.</div>';
+	}
+
+	}
+?>
