d23e74323ba0c39f34beb1efe101d7a0734465bd
alexisivr <alexisveron1993@gmail.com>
2017-06-11 13:58:25 -0300

    mod1test

--- enviarMail.php
+++ enviarMail.php
@@ -16,9 +16,8 @@
 		$to = 'alexisveron1993@gmail.com';
 		$subject = 'Presupuesto transporte ';
 
-		$body ="De: $name\n E-Mail: $email\n Mensaje:\n $message";
 // If there are no errors, send the email
-	if (mail ($to, $subject, $body, $from)) {
+	if (mail ($to, $subject, $body, $from,$value,$weight,$vol)) {
 		$result='<div class="alert alert-success">Gracias por contactarse! Responderemos a la brevedad </div>';
 	} else {
 		$result='<div class="alert alert-danger"> Hubo un inconveniente. Intente nuevamente.</div>';
